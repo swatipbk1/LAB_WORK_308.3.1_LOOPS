84940495c7ba67dbfaa222cfd229b3685a7a4393
swatipbk1 <swati.pandey.ca@gmail.com>
2024-01-17 22:05:00 -0800

    Added prob3.js file

---
+++ prob3.js
@@ -0,0 +1,10 @@
+/*Proble3-READING CSV string ,storing and printing it out to console*/
+
+let str="ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+let size=0;
+for(let val of str){
+    console.log("val", val);
+    size++;
+ 
+}
